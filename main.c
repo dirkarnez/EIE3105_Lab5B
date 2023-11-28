17cb1f9a061d6a6e1e7a7f448e3f4ba300ee7824
dirkarnez <smalldirkalex@gmail.com>
2023-11-28 22:54:33 +0800

    - complete

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ void delay(int t) {
 int main(void) {
 	//GPIO set up for PA5 (on-board LED)
 	RCC->APB2ENR |= RCC_APB2Periph_GPIOA; //Enable APB2 peripheral clock
-	GPIOA->CRL &= (unsigned int)~0x00F00000; //clear the setting
+	GPIOA->CRL &= ~0x00F00000; //clear the setting
 	GPIOA->CRL |= 0 << 22 | 2 << 20; //GPIO_Mode_Out_PP, GPIO_Speed_2MHz
 	
 	while(1) {
 		GPIOA->BSRR |= 0x20;
-		delay(3000000);
+		delay(2000000);
 		GPIOA->BRR  |= 0x20;
-		delay(3000000);
+		delay(2000000);
 	}
 }
